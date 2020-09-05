cfc9a5c7f6017747aabfb363b141f3d67ad27faa
Byron Brown <Beezy@MacBook-4.local>
2020-09-05 17:48:17 -0500

    initial commit

---
+++ build/precache-manifest.e4ffcac773943148ae2d8db3a2695e6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29bce751ebdb083bb82c0edcb4ad668",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2554646478524ee8d53",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3aa5c857e341d2f07e4b",
+    "url": "/static/js/2.2121fd6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2121fd6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2554646478524ee8d53",
+    "url": "/static/js/main.3d36ed01.chunk.js"
+  },
+  {
+    "revision": "806301a85e2f5c18aa2f",
+    "url": "/static/js/runtime-main.a23e992b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
